8a099699f4439a302e12476411dafcdf16e2a24c
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-08-28 02:23:20 +0200

    Initial commit

---
+++ src/main/java/ua/goit/notificationsapp/NotificationsAppApplication.java
@@ -0,0 +1,17 @@
+package ua.goit.notificationsapp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class NotificationsAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NotificationsAppApplication.class, args);
+
+	}
+
+}
