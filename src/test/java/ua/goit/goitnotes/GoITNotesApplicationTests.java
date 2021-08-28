07e6dd13abda475391d647128697c7e62e149d4e
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-08-28 14:15:08 +0200

    Change project name

--- src/test/java/ua/goit/goitnotes/GoITNotesApplicationTests.java
+++ src/test/java/ua/goit/goitnotes/GoITNotesApplicationTests.java
@@ -1,10 +1,10 @@
-package ua.goit.notificationsapp;
+package ua.goit.goitnotes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NotificationsAppApplicationTests {
+class GoITNotesApplicationTests {
 
 	@Test
 	void contextLoads() {
