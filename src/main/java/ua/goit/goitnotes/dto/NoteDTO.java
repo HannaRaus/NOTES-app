35e17d71f462a10271972314993ac6ff2dbf410f
Burkov1982 <burkov_daniil@i.ua>
2021-08-31 08:10:51 +0300

    Updates

--- src/main/java/ua/goit/goitnotes/dto/NoteDTO.java
+++ src/main/java/ua/goit/goitnotes/dto/NoteDTO.java
@@ -5,21 +5,13 @@ import lombok.Setter;
 
 import java.util.UUID;
 
+@Getter
+@Setter
 public class NoteDTO {
-    @Getter
-    @Setter
     private UUID id;
-    @Getter
-    @Setter
     private String title;
-    @Getter
-    @Setter
     private String content;
-    @Getter
-    @Setter
     private String accessType;
-    @Getter
-    @Setter
     private String userName;
 
     public NoteDTO(UUID id, String title, String content, String accessType, String userName) {
