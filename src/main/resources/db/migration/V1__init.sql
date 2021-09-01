6c4414925417849dfac52262b766dfe8ec51ede8
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2021-09-01 15:29:42 +0300

    correct db

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,39 +1,24 @@
+CREATE TABLE role
+(
+    id   VARCHAR(255) PRIMARY KEY,
+    name varchar(100) UNIQUE NOT NULL
+);
+
 CREATE TABLE users
 (
-    id UUID PRIMARY KEY,
-    name varchar(100) UNIQUE NOT NULL,
-    password varchar(200) NOT NULL,
-    role varchar(100) NOT NULL
+    id       VARCHAR(255) PRIMARY KEY,
+    name     varchar(100) UNIQUE NOT NULL,
+    password varchar(200)        NOT NULL,
+    role_id  VARCHAR(255)        NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role (id)
 );
 
 CREATE TABLE notes
 (
-    id UUID PRIMARY KEY,
-    title varchar(200) UNIQUE NOT NULL,
-    content varchar(10100)      NOT NULL,
-    access_type varchar(100) NOT NULL,
-    user_id UUID NOT NULL,
+    id          VARCHAR(255) PRIMARY KEY,
+    title       varchar(200)   NOT NULL,
+    content     varchar(10100) NOT NULL,
+    access_type varchar(100)   NOT NULL,
+    user_id     VARCHAR(255)     NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
-/*
-
-
-
-CREATE TABLE users
-(
-    id BINARY(16) PRIMARY KEY,
-    name varchar(100) UNIQUE NOT NULL,
-    password varchar(200) NOT NULL,
-    role varchar(100) NOT NULL
-);
-
-CREATE TABLE notes
-(
-    id BINARY(16) PRIMARY KEY,
-    title varchar(200) UNIQUE NOT NULL,
-    content varchar(10100)      NOT NULL,
-    access_type varchar(100) NOT NULL,
-    user_id BINARY(16) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id)
-);
-*/
