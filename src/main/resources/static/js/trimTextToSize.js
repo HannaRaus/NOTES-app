26c21637da3a67b5cc3b25302e0855e45e9e83ce
HannaRaus <raus.a.hanna@gmail.com>
2021-09-05 21:22:24 +0300

    refactor truncated text. add link for detailed note

--- src/main/resources/static/js/trimTextToSize.js
+++ src/main/resources/static/js/trimTextToSize.js
@@ -1,24 +1,21 @@
 //<!-- script to truncate note content and add url button for read more --!>
-function truncate (selector, maxLength) {
-    var td = document.getElementById(selector);
-	var text = td.innerHTML;
+    var maxLength = 500;
 
-    if(text.length > maxLength) {
-        //get substring with maxLength limit
-    	newText = text.substr(0,maxLength);
-        //set new text in td
-        td.innerHTML = newText;
+jQuery(document).ready(function($) {
+    $(".content").click(function() {
+        window.location = $(this).data("href");
+    });
+});
 
-        //create new a element
-	    let a = document.createElement('a');
-		var link = td.getAttribute('href');
-	    a.setAttribute('href',link);
-	    a.textContent = '...';
-        //append a element to td
-	    td.appendChild(a);
-	}
-
-}
+$(document).ready(function(){
+    $(".content").each(function(){
+        var myStr = $(this).text();
+        if($.trim(myStr).length > maxLength){
+            var newStr = myStr.substring(0, maxLength);
+            $(this).empty().html(newStr + '...');
+        }
+    });
+});
 
 
 
