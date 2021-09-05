6295cbc12e9ea48a8f4c599b3b75849a5794b92e
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2021-09-05 20:00:06 +0200

    Fix registration logic

--- src/main/resources/static/js/sendToValidateUser.js
+++ src/main/resources/static/js/sendToValidateUser.js
@@ -1,36 +1,30 @@
 function sendToValidateAndCreateUser() {
 
-    const bcrypt = require('bcrypt')
-    let password = document.querySelector(".password");
     let result = document.querySelector('.result');
-    result.innerHTML = "in work";
-    let name = document.querySelector('.name');
-    let passwordHash = bcrypt.hashSync(password, 10);
-    console.log(passwordHash);
+    let name = document.querySelector('#username');
+    let password = document.querySelector("#password");
+    console.log(name, password);
     let request = new XMLHttpRequest();
     console.log(request);
 
-
-
-    let url = "http://localhost:9999/user/registration";
+    let url = "/user/registration";
     request.open("POST", url, true);
     request.setRequestHeader("Content-Type", "application/json");
     request.responseType = 'json'
     request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status === 200) {
+        if (/*request.readyState === 4 && */request.status === 200) {
             var operationStatus = request.response;
             result.innerHTML = operationStatus;
             if (operationStatus.success === true) {
                 window.location.href = '/login';
             } else {
                 result.innerHTML = operationStatus.errors;
-                let errors = operationStatus.errors;
             }
         } else {
-            result.innerHTML = "something gone wrong"
+            result.innerHTML = request.statusText;
         }
     };
-    const data = JSON.stringify({"name": name.value, "password": passwordHash.value});
+    const data = JSON.stringify({"name": name.value, "password": password.value});
     result.innerHTML = data;
     request.send(data);
 }
